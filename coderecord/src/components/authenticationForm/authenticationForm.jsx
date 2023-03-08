0884b28e254e37a656ff0f0f0d236388e4b90df7
Raghevndra Gautam <raghvendragautam240@gmail.com>
2023-03-09 01:05:37 +0530

    Added close button

--- coderecord/src/components/authenticationForm/authenticationForm.jsx
+++ coderecord/src/components/authenticationForm/authenticationForm.jsx
@@ -3,12 +3,17 @@ import "./authenticationForm.scss";
 import "../../assets/styles/common.scss"
 import PrimaryButton from "../primaryButton/primaryButton";
 import miniLogo from "../../assets/images/logo_mini.png";
+import closeBtn from "../../assets/images/close_btn.png";
 export default function authenticationForm() {
+   
   return (
     <div className="backdrop">
         <div className="authentication-form">
             <div>
-                <div className="form-header">Sign Up</div>
+                <div className="form-header">
+                    <div>Sign Up</div>
+                    <div className="close-btn-wrapper"><img className="close-btn-image" src={closeBtn} alt="" /></div>
+                </div>
                 <div>
                     <img className="watermark" src={miniLogo} alt="" />
                 </div>
