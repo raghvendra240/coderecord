9a5963453118af0cae2f37864a40426e0abdd12f
Raghevndra Gautam <raghvendragautam240@gmail.com>
2023-03-11 12:25:54 +0530

    CE: Temp popup design

--- chrome extenstion/background/background.js
+++ chrome extenstion/background/background.js
@@ -1,9 +1,35 @@
 // Listen for tab updates
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    // Check if the tab has finished loading
-    if (changeInfo.status === 'complete') {
-      // Execute content script in tab
-       console.log('789 Website loaded', tab)
-    }
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//     // Check if the tab has finished loading
+//     if (changeInfo.status === 'complete') {
+//       chrome.notifications.create({
+//         type: 'basic',
+//         iconUrl: './icon.png',
+//         title: 'My Extension',
+//         message: 'This is a notification from My Extension!',
+//       });
+//     }
+//   });
+
+// function openPopup () {
+//     chrome.action.openPopup();
+// }
+
+function showNotification () {
+  chrome.notifications.create({
+    type: 'basic',
+    iconUrl: './icon.png',
+    title: 'My Extension',
+    message: 'This is a notification from My Extension!',
   });
-  
+}
+
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+      if (message.type === 'submittedProblem') {
+        const { problem } = message;
+        // console.log('789 problem', problem, chrome.notifications);
+          showNotification();
+        // chrome.browserAction.setPopup()
+        // openPopup();
+      }
+  });
