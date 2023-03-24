925d3ec292fe38b951b5eeddbe9d2711aab5df31
Raghevndra Gautam <raghvendragautam240@gmail.com>
2023-03-24 23:39:06 +0530

    FE: init sort

--- coderecord/src/components/Sort/Sort.jsx
+++ coderecord/src/components/Sort/Sort.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 import './Sort.scss'
+import sortIcon from '../../assets/images/sort.svg'
+import downChevron from '../../assets/images/down-chevron.svg'
 export default function Sort() {
   return (
-    <div>Sort</div>
+    <div className='sort-container'>
+      <div className='sort-header'>
+        <div className='sort-icon'>
+          <img src={sortIcon} alt="" />
+        </div>
+        <div className='sort-text'>
+          <div>Sort:</div>
+          <div className='selected'>Submission</div>
+        </div>
+        <div className='down-chevron'>
+          <img src={downChevron} alt="" />
+        </div>
+      </div>
+      {/* <div className='sort-list'></div> */}
+
+    </div>
   )
 }
