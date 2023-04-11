ae2198f26facf37900030d456d5df226be5d45e5
Raghevndra Gautam <raghvendragautam240@gmail.com>
2023-04-11 22:23:43 +0530

    FE: Added bouncing function in search

--- coderecord/src/components/Search/Search.jsx
+++ coderecord/src/components/Search/Search.jsx
@@ -4,18 +4,26 @@ import searchIcon from '../../assets/images/search_icon.svg'
 import crossIcon from '../../assets/images/cross_icon.svg'
 import mainBodyContext from '../../contexts/mainBodyContext'
 
-let onPrimaryBtnClick = function (searchText, setSearchText) {
-    if (searchText) {
-        setSearchText('');
-    } 
-}
 
 export default function Search() {
-    const {searchText, setSearchText} = useContext(mainBodyContext);
-    let onPrimaryBtnClickWrapper = () => onPrimaryBtnClick(searchText, setSearchText);
+    const {searchText,setSearchText} = useContext(mainBodyContext);
+    let onPrimaryBtnClickWrapper = (event) => {
+        document.querySelector('.search-input').value = '';
+        setSearchText('');
+    };
+    let lastTimeout = null;
+    const onInputChange = (event) => {
+        const value = event.target.value;
+        if (lastTimeout) {
+            clearTimeout(lastTimeout);
+        }
+        lastTimeout = setTimeout(() => {
+            setSearchText(value);
+        }, 500);
+    }
   return (
    <div className='search-box-container'>
-        <input type='text' className='search-input' value={searchText} onChange={(event) => {setSearchText(event.target.value)}} placeholder='Search'></input>
+        <input type='text' className='search-input' onChange={onInputChange} placeholder='Search'></input>
         <button className='search-button' onClick={onPrimaryBtnClickWrapper}>
             <img src={searchText ? crossIcon : searchIcon} alt="" />
         </button>
