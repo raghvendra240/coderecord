b6f365c7069e99b71b756c1c2f96b6dbcb394a98
Raghevndra Gautam <raghvendragautam240@gmail.com>
2023-04-12 12:09:43 +0530

    FE: Updated base URL

--- coderecord/src/utils/globalConstants.js
+++ coderecord/src/utils/globalConstants.js
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://localhost:5000/api';
+export const BASE_URL = 'https://rich-elk-top-coat.cyclic.app/api';
+// export const BASE_URL = 'http://localhost:5000/api';
 export const LOCAL_STORAGE_KEY = 'coderecordUserData';
 export const LEETCODE = 'leetcode';
 export const GFG = 'gfg';
